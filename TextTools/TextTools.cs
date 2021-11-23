1f7285e6c9397ed761fc873a4db453b37a7cd882
DAVID PLANDOR <david.plandor@cz.abb.com>
2021-11-23 11:08:39 +0100

    load bug file on button click

--- TextTools/TextTools.cs
+++ TextTools/TextTools.cs
@@ -2,9 +2,9 @@
 {
     public class TextTools
     {   
-        public static Dictionary<string, int> TopTenWords(string fileString)
+        public static Dictionary<string, int> TopTenWords(string fileString, string splitBy = " ")
         {
-            var wordsCount = fileString.Split(" ").GroupBy(x => x).Select(g => (Word: g.Key, Count: g.Count())).OrderByDescending(p => p.Count).Take(10);
+            var wordsCount = fileString.Split(splitBy).GroupBy(x => x).Select(g => (Word: g.Key, Count: g.Count())).OrderByDescending(p => p.Count).Take(10);
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (var tuple in wordsCount)
             {
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,12 +27,26 @@ namespace WpfTextGUI
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            foreach (var file in Directory.GetFiles("Books"))
+            /*foreach (var file in Directory.GetFiles("Books"))
             {
                 var countWords = TextTools.TextTools.TopTenWords(File.ReadAllText(file));
                 txbInfo.Text += Path.GetFileName(file) + Environment.NewLine;
-               countWords.ToList().ForEach(x => txbInfo.Text += x.Key + " " + x.Value + Environment.NewLine);
+                countWords.ToList().ForEach(x => txbInfo.Text += x.Key + " " + x.Value + Environment.NewLine);
+            }*/
+        }
+
+        private void bLoad_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            txbInfo.Text = "";
+            var bigFilesDir = @"C:\Users\czdapla\source\BigFiles";
+            foreach (var file in Directory.GetFiles(bigFilesDir))
+            {
+                var countWords = TextTools.TextTools.TopTenWords(File.ReadAllText(file), Environment.NewLine);
+                txbInfo.Text += Environment.NewLine + Path.GetFileName(file) + Environment.NewLine;
+                countWords.ToList().ForEach(x => txbInfo.Text += x.Key + " " + x.Value + Environment.NewLine);
             }
+            Mouse.OverrideCursor = Cursors.Arrow;
         }
     }
 }
